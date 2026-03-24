5e80db7cc3c74361519a976ebce3fa282215a024
1vickykumar <hivickykumar96@gmail.com>
2026-03-24 17:13:35 +0530

    urlSecure Cors origin remove

--- src/app.js
+++ src/app.js
@@ -5,12 +5,7 @@ import messageRouter from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-app.use(
-  cors({
-    origin: "https://dhrma.store",
-    credentials: true
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
